525589a361be5b89f4f9cf529aa76f7c61245e47
Jasmin <ramosjasminsabelita@gmail.com>
2023-04-16 21:33:55 -0600

    Final Assignment

--- src/EmptyLotion.js
+++ src/EmptyLotion.js
@@ -1,5 +1,9 @@
-function EmptyLotion() {
-    return <div id="empty-holder">Select a note, or create a new one.</div>;
-  }
+// function EmptyLotion() {
+//     return <div id="empty-holder">Select a note, or create a new one.</div>;
+//   }
   
+// export default EmptyLotion;
+
+const EmptyLotion = () => <div id="empty-holder">Select a note, or create a new one.</div>;
+
 export default EmptyLotion;
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,10 @@
-const currentDate = () => {
-    const date = new Date();
-    return new Date(date.getTime() - date.getTimezoneOffset() * 60000)
-    .toJSON()
-    .substring(0, 19);
-}
+// const currentDate = () => {
+//     const date = new Date();
+//     return new Date(date.getTime() - date.getTimezoneOffset() * 60000)
+//     .toJSON()
+//     .substring(0, 19);
+// }
 
-export { currentDate };
+// export { currentDate };
+
+export const currentDate = () => new Date().toISOString().slice(0, 19).replace("T", " ");
