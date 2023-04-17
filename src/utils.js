6154f98b370b1c0c59207c23d52d1a7a2f1b2604
Jasmin <ramosjasminsabelita@gmail.com>
2023-04-16 21:39:00 -0600

    Final

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,9 @@
-// const currentDate = () => {
-//     const date = new Date();
-//     return new Date(date.getTime() - date.getTimezoneOffset() * 60000)
-//     .toJSON()
-//     .substring(0, 19);
-// }
+const currentDate = () => {
+    const date = new Date();
+    return new Date(date.getTime() - date.getTimezoneOffset() * 60000)
+    .toJSON()
+    .substring(0, 19);
+}
 
-// export { currentDate };
+export { currentDate };
 
-export const currentDate = () => new Date().toISOString().slice(0, 19).replace("T", " ");
